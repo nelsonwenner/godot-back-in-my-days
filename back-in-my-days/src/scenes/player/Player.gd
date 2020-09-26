6350edc0bd97b7f5d848296ef63696f05d8a7499
wenner <nelsondiasdemedeiros@gmail.com>
2020-09-26 18:36:38 -0300

    feat: create singletons server and, add move in player

---
+++ back-in-my-days/src/scenes/player/Player.gd
@@ -0,0 +1,27 @@
+extends KinematicBody2D
+
+var SPEED = 130
+
+
+func _ready():
+	pass
+
+
+func _process(_delta):
+	pass
+
+
+func _physics_process(_delta):
+	var motion = Vector2(0,0)
+	
+	if Input.is_action_pressed("player_up"):
+		motion.y -= SPEED
+	elif Input.is_action_pressed("player_down"):
+		motion.y += SPEED
+	if Input.is_action_pressed("player_right"):
+		motion.x += SPEED
+	elif Input.is_action_pressed("player_left"):
+		motion.x -= SPEED
+			
+	motion = motion.normalized()
+	motion = move_and_slide(motion * SPEED)
